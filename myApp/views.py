0a050979138c456d130b0261c10ee418f55f9dec
Jvictorio09 <juliavictorio16@gmail.com>
2025-11-12 13:22:21 +0800

    updated

--- myApp/views.py
+++ myApp/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse("Hello, world! This is myApp inside myProject 🚀")
+    return render(request, "home.html")
